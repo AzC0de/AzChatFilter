18b397d7b756220a46a801309af93530a8ee8361
AzC0de <35283766+AzC0de@users.noreply.github.com>
2023-04-18 00:07:05 -0500

    Update 0.2

--- src/main/java/us/azcode/azchatfilter/listeners/ChatListener.java
+++ src/main/java/us/azcode/azchatfilter/listeners/ChatListener.java
@@ -16,42 +16,39 @@ import java.util.regex.Pattern;
 public class ChatListener implements Listener {
     private final AzChatFilter plugin;
     private final Set<String> lastMessages;
-    private final Pattern urlPattern;
 
     public ChatListener(AzChatFilter plugin) {
         this.plugin = plugin;
         this.lastMessages = new HashSet<>();
-        this.urlPattern = Pattern.compile(
-                "((?:(?:http|https|Http|Https|ftp|Ftp)://|(?:www\\.|ftp\\.)[\\w_-]+\\.)" +
-                "[a-zA-Z0-9-_]+(?:\\.[a-zA-Z0-9-_]+)*(?::\\d+)?(?:/\\S*)?)");
+    }
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        String message = event.getMessage();
+
+        if (player.hasPermission("azchatfilter.bypass")) {
+            return;
         }
 
-        @EventHandler
-        public void onChat(AsyncPlayerChatEvent event) {
-            Player player = event.getPlayer();
-            String message = event.getMessage();
+        List<String> bannedWords = plugin.getConfig().getStringList("banned_words");
+        boolean censorWithAsterisks = plugin.getConfig().getBoolean("censor_with_asterisks");
 
-            if (player.hasPermission("azchatfilter.bypass")) {
-                return;
-            }
-
-            List<String> bannedWords = plugin.getConfig().getStringList("banned_words");
-            boolean censorWithAsterisks = plugin.getConfig().getBoolean("censor_with_asterisks");
-
-            for (String word : bannedWords) {
-                Pattern pattern = Pattern.compile("\\b" + word + "\\b", Pattern.CASE_INSENSITIVE);
-                Matcher matcher = pattern.matcher(message);
-                if (matcher.find()) {
-                    if (censorWithAsterisks) {
-                        message = matcher.replaceAll(getAsterisks(word.length()));
-                    } else {
-                        event.setCancelled(true);
-                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getMessages().getString("banned_word_message")));
-                        return;
-                    }
+        for (String word : bannedWords) {
+            Pattern pattern = Pattern.compile("\\b" + word + "\\b", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(message);
+            if (matcher.find()) {
+                if (censorWithAsterisks) {
+                    message = matcher.replaceAll(getAsterisks(word.length()));
+                } else {
+                    event.setCancelled(true);
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getMessages().getString("banned_word_message")));
+                    return;
                 }
             }
+        }
 
+        if (plugin.getConfig().getBoolean("enable_message_repeat_check")) {
             if (lastMessages.contains(player.getUniqueId().toString() + message)) {
                 event.setCancelled(true);
                 player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getMessages().getString("repeat_message")));
@@ -59,33 +56,16 @@ public class ChatListener implements Listener {
             } else {
                 lastMessages.add(player.getUniqueId().toString() + message);
             }
-
-            Matcher urlMatcher = urlPattern.matcher(message);
-            if (urlMatcher.find()) {
-                String url = urlMatcher.group();
-                List<String> whitelistedUrls = plugin.getConfig().getStringList("whitelisted_urls");
-                boolean isWhitelisted = false;
-                for (String whitelistedUrl : whitelistedUrls) {
-                    if (url.contains(whitelistedUrl)) {
-                        isWhitelisted = true;
-                        break;
-                    }
-                }
-
-                if (!isWhitelisted) {
-                    event.setCancelled(true);
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getMessages().getString("url_not_allowed_message")));
-                    return;
-                }
-            }
-
-            event.setMessage(message); 
         }
-        private String getAsterisks(int length) {
-            StringBuilder asterisks = new StringBuilder();
-            for (int i = 0; i < length; i++) {
-                asterisks.append("*");
-            }
-            return asterisks.toString();
+
+        event.setMessage(message);
+    }
+
+    private String getAsterisks(int length) {
+        StringBuilder asterisks = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            asterisks.append("*");
         }
+        return asterisks.toString();
+    }
 }
